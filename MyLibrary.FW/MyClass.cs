a2405e534934fe6494918df47d04b2fe2fec8350
nirajp82 <61636643+nirajp82@users.noreply.github.com>
2022-03-03 16:06:12 -0500

    Add files via upload

---
+++ MyLibrary.FW/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public class MyClass
+    {
+        public static string Greet(string name) => $"Hello {name} from Framework Library. Static Method";
+        public string Greet2(string name) => $"Hello {name} from Framework Library. Instance Method";
+    }
+}
---
+++ UtilLib/AssemblyExt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UtilLib
+{
+    public class AssemblyExt
+    {
+        public static string GetAssemblyInfo(Type type)
+        {
+            return $"AssemblyQualifiedName: {type.AssemblyQualifiedName}- Location:{type.Assembly.Location}";
+        }
+    }
+}
