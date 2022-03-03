b24c8a5d2da8147076736824fd58051802e0ee53
nirajp82 <61636643+nirajp82@users.noreply.github.com>
2022-03-03 15:51:25 -0500

    Add files via upload

---
+++ MyLibrary.Client/LibClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UtilLib;
+
+namespace MyLibrary.Client
+{
+    public class LibClient
+    {
+        public static List<string> Greet(string name) 
+        {
+            List<string> result = new List<string>();
+            result.Add(MyLibrary.MyClass.Greet("Raj"));
+            result.Add(new MyLibrary.MyClass().Greet2("Raj"));
+            result.Add(AssemblyExt.GetAssemblyInfo(typeof(MyLibrary.MyClass)));
+            return result;
+        }
+    }
+}
---
+++ MyLibrary/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public class MyClass
+    {
+        public static string Greet(string name) => $"Hello {name} from Standard Library. Static Method";
+        public string Greet2(string name) => $"Hello {name} from Standard Library. Instance Method";
+    }
+}
